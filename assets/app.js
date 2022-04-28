2069f8c5de50af9a8ea09cc326c321d81a94be96
Yakhlef1981 <samir.yakhlef30@gmail.com>
2022-04-28 14:11:41 +0200

    upload des assets du css et du scss du thème

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import bsCustomFileInput from 'bs-custom-file-input';
-import 'bootstrap';
-import './js/functions';
+//import 'bootstrap';
+//import './js/functions';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
 bsCustomFileInput.init();
